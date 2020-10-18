3e04da1daeef955b95f842282a3963a26efed915
David Ainslie <dainslie@gmail.com>
2020-10-18 14:54:29 +0100

    Extra configuration

--- src/main/scala/com/backwards/config/Config.scala
+++ src/main/scala/com/backwards/config/Config.scala
@@ -1,17 +1,19 @@
 package com.backwards.config
 
-import cats.implicits.catsSyntaxTuple2Semigroupal
+import cats.implicits._
 import com.backwards.config.ini.Mappable
 import com.backwards.config.ini.Mapping.to
 
-final case class Config(common: Common, ftp: Ftp)
+final case class Config(common: Common, ftp: Ftp, http: Http)
 
 object Config {
   implicit val configMappable: Mappable[Config] =
     (m: Map[String, Map[String, Any]]) =>
-      (to[Common].from(m("common")), to[Ftp].from(m("ftp"))).mapN(Config.apply)
+      (to[Common].from(m("common")), to[Ftp].from(m("ftp")), to[Http].from(m("http"))).mapN(Config.apply)
 }
 
 final case class Common(basic_size_limit: Long, student_size_limit: Long, paid_users_size_limit: Long, path: String)
 
 final case class Ftp(name: String, path: String, enabled: Boolean)
+
+final case class Http(name: String, path: String)
--- src/test/scala/com/backwards/config/ApplySpec.scala
+++ src/test/scala/com/backwards/config/ApplySpec.scala
@@ -9,13 +9,15 @@ class ApplySpec extends AnyWordSpec with Matchers {
   "Apply" should {
     "provide ADT attribute access via 'apply' function" in {
       val common = Common(basic_size_limit = 10, student_size_limit = 11, paid_users_size_limit = 12, path = "some-path")
-      val ftp = Ftp("my-name", "my-path", enabled = true)
-      val config = Config(common, ftp)
+      val ftp = Ftp("ftp-name", "ftp-path", enabled = true)
+      val http = Http("http-name", "http-path")
+
+      val config = Config(common, ftp, http)
 
       implicitly[Apply[Ftp]] mustBe an [Apply[_]]
 
       val name: Option[String] = config.ftp("name")
-      name mustBe "my-name".some
+      name mustBe "ftp-name".some
 
       config.ftp("non-existing") mustBe none
     }
